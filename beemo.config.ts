59b3c3d4f6a9b91c5a00c268f5ac94622eafd82d
Miles Johnson <mileswjohnson@gmail.com>
2024-05-08 16:45:40 -0700

    fix: Fix test running in watch mode.

--- beemo.config.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-default-export
-export default {
-	drivers: ['babel', 'eslint', 'jest', 'prettier', 'typescript'],
-	module: '@beemo/dev',
-	settings: {
-		node: true,
-	},
-};
